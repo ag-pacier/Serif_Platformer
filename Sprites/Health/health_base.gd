245e02b928ebc2201f67fc7ca882e5727dbdb5a2
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-04-06 15:59:43 -0400

    Organized/implemented health items

--- Sprites/Health/health_base.gd
+++ Sprites/Health/health_base.gd
@@ -2,29 +2,33 @@ extends Pickup
 class_name Health
 
 @export var heal_amount: int = 1
+var heal_label: String
 
 func _ready():
-	super()
 	if pickup_name == null:
 		pickup_name = "HealthItem" + str(get_rid())
 	worth = 5
+	if heal_amount > 0:
+		heal_label = "[color=green]+" + str(heal_amount) + "[/color]"
+		$AnimatedSprite2D.play("good")
+	else:
+		heal_label = "[color=red]" + str(heal_amount) + "[/color]"
+		$AnimatedSprite2D.play("bad")
+	$HealLabel.parse_bbcode(heal_label)
 	$SignificantParticles.visible = false
+	super()
 
 func _physics_process(delta):
+	super(delta)
 	if $HealLabel.visible == true:
 		$HealLabel.position.y -= 25 * delta
 		$HealLabel.scale -= Vector2(0.5, 0.5) * delta
 
 func _on_body_entered(body):
-	var heal_label: String
-	if heal_amount > 0:
-		heal_label = "[color=green]+" + str(heal_amount) + "[/color]"
-	else:
-		heal_label = "[color=red]" + str(heal_amount) + "[/color]"
-	$HealLabel.parse_bbcode(heal_label)
 	super(body)
 	$HealLabel.visible = true
-	
+	if body.is_in_group("MainC"):
+		body.change_health(heal_amount)
 
 # When the pickup particles disappear then we can get rid of the object
 func _on_pickup_particles_finished():
