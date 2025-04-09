ca3b1468677c1013b7fc0aee99e99c4e39960594
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-04-09 10:44:32 -0400

    Simplified trigger again/more

--- LevelStuff/_trigger/trigger_base.gd
+++ LevelStuff/_trigger/trigger_base.gd
@@ -4,9 +4,5 @@ class_name TriggerArea
 # signal to emit when triggered
 signal trigger_trip
 
-func _ready():
-	$CollisionShape2D.set_deferred("disabled", false)
-
-
 func _on_body_entered(body):
 	emit_signal("trigger_trip", body)
