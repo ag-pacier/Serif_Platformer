8e6ef7e502340b4c1b7c870152350ded6742ec3c
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-04-13 19:01:36 -0400

    Usability tweak for settings
    Allow using the left and right buttons to change the values of the sliders

--- UI/PauseMenu/SubMenus/set_menu.gd
+++ UI/PauseMenu/SubMenus/set_menu.gd
@@ -10,6 +10,38 @@ func _ready():
 	$MusicVolume/MusSlider.set_value_no_signal(AudioServer.get_bus_volume_db(1))
 	super()
 
+func _process(_delta: float):
+	if Input.is_action_just_released("left"):
+		var slider = null
+		match cur_item:
+			0:
+				slider = $TopVolume/MasSlider
+			1:
+				slider = $EffectVolume/EffSlider
+			2:
+				slider = $MusicVolume/MusSlider
+			_:
+				slider = null
+		if not slider == null:
+			var cur_value = slider.value
+			if cur_value > -80.0:
+				slider.value = slider.value - 5.0
+	if Input.is_action_just_released("right"):
+		var slider = null
+		match cur_item:
+			0:
+				slider = $TopVolume/MasSlider
+			1:
+				slider = $EffectVolume/EffSlider
+			2:
+				slider = $MusicVolume/MusSlider
+			_:
+				slider = null
+		if not slider == null:
+			var cur_value = slider.value
+			if cur_value < 6.0:
+				slider.value = slider.value + 5.0
+
 func _on_mus_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
 		AudioServer.set_bus_volume_db(1, $MusicVolume/MusSlider.value)
