8dc5cf767a45d19acd6b7ad1e02dfb81dde9a970
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-10-13 11:11:48 -0400

    reorg scene for better utility

--- Sprites/KitKit/kitkit.gd
+++ Sprites/KitKit/kitkit.gd
@@ -1,8 +1,23 @@
-extends AnimatedSprite2D
+extends StaticBody2D
 class_name KitKit
+
+## Sleeping on spawn
+@export var sleeping: bool = false
 
 # Mood sprite
 @onready var mood_bub = preload("res://Sprites/MoodBubble/MoodBubble.tscn")
+
+# If Satrio is nearby
+@onready var satrio_near: bool
+
+func _ready() -> void:
+	if sleeping:
+		toggle_z(true)
+		$AnimatedSprite2D.animation.play("asleep")
+	else:
+		toggle_z(false)
+		$AnimatedSprite2D.animation.play("sitblank")
+		
 
 func toggle_z(active: bool) -> void:
 	$EmoteAnchor/Zzs.emitting = active
@@ -14,3 +29,17 @@ func show_mood(mood: int) -> void:
 	var new_mood = mood_bub.instantiate()
 	$EmoteAnchor.add_child(new_mood)
 	new_mood.emote(mood, true)
+
+
+func _on_nearby_region_body_entered(body: Node2D) -> void:
+	if body.is_in_group("MainC"):
+		satrio_near = true
+
+
+func _on_nearby_region_body_exited(body: Node2D) -> void:
+	if body.is_in_group("MainC"):
+		satrio_near = false
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	pass # Replace with function body.
