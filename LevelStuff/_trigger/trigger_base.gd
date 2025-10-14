f9afa2d4992ee5983d0635ee65ec65313658adfe
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-10-14 12:57:31 -0400

    Rebuilt trigger to be more useful

--- LevelStuff/_trigger/trigger_base.gd
+++ LevelStuff/_trigger/trigger_base.gd
@@ -1,8 +1,32 @@
 extends Area2D
 class_name TriggerArea
 
-# signal to emit when triggered
-signal trigger_trip
+## Name for trigger
+@export var trigger_name: String = ""
 
-func _on_body_entered(body):
-	emit_signal("trigger_trip", body)
+## If the trigger should be disabled after being triggered
+@export var one_time: bool = false
+
+## Whether this item is a space for Satrio to do a contextual action
+@export var contextual: bool = false
+
+#Signal to send on trigger
+signal trigger
+
+func _ready() -> void:
+	if contextual:
+		set_collision_mask_value(7, false)
+		if trigger_name == "":
+			push_error("Triggers for context need a name!")
+
+func _on_trigger(body: Node2D) -> void:
+	if contextual:
+		body.toggle_context(true, trigger_name)
+	else:
+		if one_time:
+			$CollisionShape2D.set_deferred("disabled", true)
+		emit_signal("trigger", trigger_name)
+
+func _on_body_exited(body: Node2D) -> void:
+	if contextual:
+		body.toggle_context(false, "")
