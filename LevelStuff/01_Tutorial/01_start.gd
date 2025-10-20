ab261ee1d35743c6379658e2eee962c915ecaed4
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-10-20 13:59:43 -0400

    Showing how to move forward
    Unless someone already knows :)

--- LevelStuff/01_Tutorial/01_start.gd
+++ LevelStuff/01_Tutorial/01_start.gd
@@ -2,6 +2,18 @@ extends LevelBase
 
 func _ready():
 	super()
+	$Doors/StartTest/RInput.set_icon("right", "keyboard")
 
 func _satrio_context(context: StringName) -> void:
 	super(context)
+
+
+func _on_start_test_trigger() -> void:
+	if $StartTimer.is_stopped():
+		$Doors/StartTest/RInput.set_deferred("visible", false)
+	else:
+		$StartTimer.stop()
+
+
+func _on_start_timer_timeout() -> void:
+	$Doors/StartTest/RInput.set_deferred("visible", true)
