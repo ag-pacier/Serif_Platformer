c7b4b1674fa1c3f5a37e06225de969858a885cb7
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2025-10-20 15:51:54 -0400

    Built out tutorial more
    Need to create jump icon as it is bigger than the other buttons, included prints for now. Re did background I deleted out of frustration from before.

--- LevelStuff/01_Tutorial/01_start.gd
+++ LevelStuff/01_Tutorial/01_start.gd
@@ -3,6 +3,7 @@ extends LevelBase
 func _ready():
 	super()
 	$Doors/StartTest/RInput.set_icon("right", "keyboard")
+	$Doors/LeftSymbol/LInput.set_icon("left", "keyboard")
 
 func _satrio_context(context: StringName) -> void:
 	super(context)
@@ -17,3 +18,26 @@ func _on_start_test_trigger() -> void:
 
 func _on_start_timer_timeout() -> void:
 	$Doors/StartTest/RInput.set_deferred("visible", true)
+
+
+func _on_left_symbol_trigger() -> void:
+	if $Doors/LeftSymbol/LInput.visible:
+		$Doors/LeftSymbol/LInput.set_deferred("visible", false)
+	else:
+		$LeftTimer.stop()
+
+
+func _on_left_symbol_start_trigger() -> void:
+	$LeftTimer.start()
+
+
+func _on_left_timer_timeout() -> void:
+	$Doors/LeftSymbol/LInput.set_deferred("visible", true)
+
+
+func _on_jump_symbol_trigger() -> void:
+	print("Jump symbol tripped but not made yet")
+
+
+func _on_jump_done_trigger() -> void:
+	print("Jump symbol turned off!")
