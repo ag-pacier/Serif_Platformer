d74458604440fe098a501b624489bc36effca2d2
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2026-07-30 12:30:08 -0400

    Update effects

--- UI/Menu2.0/menubase.gd
+++ UI/Menu2.0/menubase.gd
@@ -13,9 +13,6 @@ class_name MenuI
 # Menu Dictionary
 @onready var menu_item_list: Dictionary
 
-# Signal on menu action
-signal activation(selection_name)
-
 # Node to put entries
 @onready var item_list: VBoxContainer = get_node("MTitle/ItemList")
 
@@ -36,25 +33,19 @@ func _ready() -> void:
 		new_entry.text = entry
 		item_list.add_child(new_entry)
 		menu_item_list.set(entry, new_entry)
-	
-	var final_entry = Label.new()
-	if top_level_menu:
-		final_entry.text = "Exit"
-		menu_item_list.set("Exit", final_entry)
-	else:
-		final_entry.text = "Prev"
-		menu_item_list.set("Prev", final_entry)
-	item_list.add_child(final_entry)
-	
 
 func move_ind(previous: bool = false) -> void:
+	var cur_item: Label = menu_item_list.get(entries[current_selection])
+	cur_item.label_settings = null
 	if previous:
 		if current_selection == 0:
-			current_selection = len(item_list)
+			current_selection = len(menu_item_list)
 		else:
 			current_selection -= 1
 	else:
-		if current_selection == len(item_list):
+		if current_selection == len(menu_item_list):
 			current_selection = 0
 		else:
 			current_selection += 1
+	cur_item = menu_item_list.get(entries[current_selection])
+	cur_item.label_settings = current_effect
