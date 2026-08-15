a7ff1290cd8d884e838ea1b1ceceecba272b6922
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2026-08-14 20:49:34 -0400

    added more kits, fixed minor spelling

--- LevelStuff/01_Tutorial/01_start.gd
+++ LevelStuff/01_Tutorial/01_start.gd
@@ -76,4 +76,7 @@ func _on_kit_kit_kitty_signal(signal_name: Variant) -> void:
 	elif signal_name == "seventh":
 		$Satrio.force_talking("Kit", "There are items scattered about the world. Some provide benefits, others act as guides or progress markers.")
 	elif signal_name == "eighth":
-		$Satrio.force_talking("Kit", "Your agility should let you leap against and up walls. Going down isn't always a bad though.")
+		$Satrio.force_talking("Kit", "Your agility should let you leap against and up walls. Going down isn't always bad though.")
+	elif signal_name == "ninth":
+		$Satrio.force_talking("Kit", "Heh... I didn't think you'd notice this nook. Kudos!")
+		
